95c10b559626d7b5954a41bb24d196872e2fab51
Daniel <dannyjaygil@gmail.com>
2023-09-21 13:29:11 +0300

    Add Map using react-leaflet
    I used the react-leaflet library to add a map into my application. I also made the map display markers of the cities from the list.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,21 +1,43 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useCities } from "../../contexts/CitiesContext";
 
 function Map() {
+  const { cities } = useCities();
+
   const navigate = useNavigate();
 
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
+  const mapPosition = [40, 0];
+
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>{lat}</h1>
-      <h1>{lng}</h1>
-      <button onClick={() => setSearchParams({ lat: 20, lng: 89 })}>
-        test
-      </button>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
