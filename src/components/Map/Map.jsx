039d9077e1260fda4b3497971bcbdef6ac43a778
Daniel <dannyjaygil@gmail.com>
2023-09-21 13:40:17 +0300

    Make map change center once city is selected
    Once a user clicks on a city from the list, the map will pan and center to that selected city. Also, once the user exits back into the list, the previously selected city will stay there until the next one is selected. I used a useEffect hook to synchronise the map position. Therefore every time the City ID changes in the URL, the lat and lng variables will be updated and the useEffect will set the mapPosition to the new lat and lng coordinates.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,8 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useCities } from "../../contexts/CitiesContext";
+import { useEffect, useState } from "react";
 
 function Map() {
   const { cities } = useCities();
@@ -12,7 +13,14 @@ function Map() {
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
-  const mapPosition = [40, 0];
+  const [mapPosition, setMapPosition] = useState([lat || 40, lng || -5]);
+
+  useEffect(
+    function () {
+      if (lat && lng) setMapPosition([lat, lng]);
+    },
+    [lat, lng]
+  );
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
@@ -37,9 +45,17 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
