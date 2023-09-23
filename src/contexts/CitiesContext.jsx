ee14938b25cfa656eebf8ff3cae50b4bae9914e1
Daniel <dannyjaygil@gmail.com>
2023-09-23 07:39:53 +0300

    Refactor CitiesContext to use useReducer hook

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,40 +1,86 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const CitiesContext = createContext();
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case "cities/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+
+    case "city/loaded":
+      return {
+        ...state,
+        currentCity: action.payload,
+      };
+
+    case "city/created":
+      return {
+        ...state,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+
+    case "city/deleted":
+      return {
+        ...state,
+        cities: state.cities.filter(
+          (city) => String(city.id) !== String(action.payload)
+        ),
+      };
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(function () {
     async function fetchCities() {
+      dispatch({ type: "loading" });
       try {
-        setIsLoading(true);
         const res = await fetch("/data/cities.json");
         const data = await res.json();
         const { cities } = data;
-        setCities(cities);
+        dispatch({ type: "cities/loaded", payload: cities });
       } catch {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
+        throw new Error("There was a problem loading cities");
       }
     }
     fetchCities();
   }, []);
 
   function getCity(id) {
-    cities.map((city) => String(city.id) === id && setCurrentCity(city));
+    cities.map(
+      (city) =>
+        String(city.id) === id &&
+        dispatch({ type: "city/loaded", payload: city })
+    );
   }
 
   function addCity(newCity) {
-    setCities((cities) => [...cities, newCity]);
-    setCurrentCity(newCity);
+    dispatch({ type: "city/created", payload: newCity });
   }
 
   function deleteCity(id) {
-    setCities(cities.filter((city) => String(city.id) !== String(id)));
+    dispatch({ type: "city/deleted", payload: id });
   }
 
   return (
